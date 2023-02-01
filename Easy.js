1bd862d19f1f8673fbd099efbb42205d50c761e4
Keerthana Yellapragada <keerthanay@gmail.com>
2023-01-31 16:34:58 -0800

    solved prob 3

--- Easy.js
+++ Easy.js
@@ -36,3 +36,18 @@ function isValidSubsequence(array, sequence) {
 
 
 // ********************************************************************************************
+
+//  ISPALINDROME
+function isPalindrome(string) {
+    let reversed = ""
+
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversed += string[i]
+    }
+
+    return reversed === string
+}
+
+// ********************************************************************************************
+
+//  CAESER'S CIPHER
