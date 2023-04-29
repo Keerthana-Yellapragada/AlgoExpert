20e86f07934701b776485380af362fdc5b782096
Keerthana Yellapragada <keerthanay@gmail.com>
2023-04-29 11:12:30 +0530

    notes

--- TypeScript/Notes.ts
+++ TypeScript/Notes.ts
@@ -57,3 +57,50 @@ let customerId = cid as number
 function addNum(x: number, y: number): number {
   return x + y
 }
+// Void
+function log(message: string | number): void {
+  console.log(message)
+}
+
+// Interfaces
+interface UserInterface {
+  readonly id: number
+  name: string
+  age?: number
+}
+
+const user1: UserInterface = {
+  id: 1,
+  name: 'John',
+}
+
+interface MathFunc {
+  (x: number, y: number): number
+}
+
+const add: MathFunc = (x: number, y: number): number => x + y
+const sub: MathFunc = (x: number, y: number): number => x - y
+
+interface PersonInterface {
+  id: number
+  name: string
+  register(): string
+}
+
+// Classes
+class Person implements PersonInterface {
+  id: number
+  name: string
+
+  constructor(id: number, name: string) {
+    this.id = id
+    this.name = name
+  }
+
+  register() {
+    return `${this.name} is now registered`
+  }
+}
+
+const brad = new Person(1, 'Brad Traversy')
+const mike = new Person(2, 'Mike Jordan')
