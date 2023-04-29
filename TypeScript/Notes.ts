2c499a7e2c9c3fd331ee1739b758cfa814ac87a5
Keerthana Yellapragada <keerthanay@gmail.com>
2023-04-29 11:12:43 +0530

    notes

--- TypeScript/Notes.ts
+++ TypeScript/Notes.ts
@@ -104,3 +104,27 @@ class Person implements PersonInterface {
 
 const brad = new Person(1, 'Brad Traversy')
 const mike = new Person(2, 'Mike Jordan')
+
+// Subclasses
+class Employee extends Person {
+  position: string
+
+  constructor(id: number, name: string, position: string) {
+    super(id, name)
+    this.position = position
+  }
+}
+
+const emp = new Employee(3, 'Shawn', 'Developer')
+
+
+
+// Generics
+function getArray<T>(items: T[]): T[] {
+  return new Array().concat(items)
+}
+
+let numArray = getArray<number>([1, 2, 3, 4])
+let strArray = getArray<string>(['brad', 'John', 'Jill'])
+
+strArray.push(1) // Throws error
