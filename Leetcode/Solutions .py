1f5f748ddbad1d942d399bda46f20b4f4a98c5b5
Keerthana Yellapragada <keerthanay@gmail.com>
2023-05-06 16:13:36 +0530

    solved generate paranthesis

--- Leetcode/Solutions .py
+++ Leetcode/Solutions .py
@@ -294,3 +294,24 @@ def nextPermutation(self, nums):
                         nums[k], nums[j] = nums[j], nums[k]
                         return nums
         return nums.reverse()
+
+
+
+# 22. Generate Parentheses
+ def generateParenthesis(self, n: int) -> List[str]:
+
+
+        def helper(ans, s, left, right):
+            if left==0 and right==0:
+                ans.append(s)
+
+            if left>0:
+                helper(ans, s+'(', left-1, right)
+
+            if right>0 and left<right:
+                helper(ans, s+')', left, right-1)
+
+        ans = []
+        helper(ans, '', n, n)
+
+        return ans
