fe5c3a142ff8d7fb8d151652144b81b6bf346bbb
Keerthana Yellapragada <keerthanay@gmail.com>
2023-05-06 16:11:02 +0530

    js trivia problems

---
+++ Leetcode/Solutions.js
@@ -0,0 +1,31 @@
+// Array Prototype Last
+Array.prototype.last = function () {
+    return this[this.length - 1] ? ? -1;
+};
+
+
+// 2625. Flatten Deeply Nested Array
+var jsonStringify = function (object) {
+    if (object === null) {
+        return 'null';
+    }
+    if (typeof object === 'string') {
+        // return the string value surrounded by double quotes.
+        return '"' + object + '"';
+    }
+    if (typeof object === 'number' || typeof object === 'boolean') {
+        // return its string representation.
+        return String(object);
+    }
+    if (Array.isArray(object)) {
+        // Recursively convert each item to a JSON string and join them with commas.
+        const items = object.map(item => jsonStringify(item)).join(',');
+        return '[' + items + ']';
+    }
+    // Recursively convert each value to a JSON string and pair it with the corresponding key.
+    if (typeof object === 'object') {
+        const keys = Object.keys(object);
+        const items = keys.map(key => '"' + key + '":' + jsonStringify(object[key]));
+        return '{' + items.join(',') + '}';
+    }
+};
