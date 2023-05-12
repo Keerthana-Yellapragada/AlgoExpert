5924513f015dd6fbcb14aefec4b75a0d910ca1e7
Keerthana Yellapragada <keerthanay@gmail.com>
2023-05-12 11:08:01 +0530

    refactoring

--- AlgoExprt/Easy.js
+++ AlgoExprt/Easy.js
@@ -42,11 +42,15 @@ function isPalindrome(string) {
     let reversed = ""
 
     for (let i = string.length - 1; i >= 0; i--) {
+
         reversed += string[i]
+
     }
 
     return reversed === string
 }
+
+
 
 // ********************************************************************************************ow t
 
