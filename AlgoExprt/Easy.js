449c8d7108ae4607c47ba4400c82eb61fd171bf9
Keerthana Yellapragada <keerthanay@gmail.com>
2023-05-12 11:08:21 +0530

    refactoring

--- AlgoExprt/Easy.js
+++ AlgoExprt/Easy.js
@@ -4,9 +4,13 @@ function twoNumberSum(array, targetSum) {
     let newArray = []
 
     for (let i = 0; i < array.length; i++) {
+
         for (let j = i + 1; j < array.length; j++) {
+
             if (array[i] + array[j] === targetSum) {
+
                 newArray.push(array[i], array[j])
+
             }
         }
     }
