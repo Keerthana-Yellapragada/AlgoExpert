b731fc054c738976dd5d21c4bcfa26554d1478ef
Keerthana Yellapragada <keerthanay@gmail.com>
2023-05-21 12:19:48 +0530

    interfaces

--- TypeScript/Notes.ts
+++ TypeScript/Notes.ts
@@ -95,6 +95,7 @@ interface MathFunc {
   (x: number, y: number): number
 }
 
+
 const add: MathFunc = (x: number, y: number): number => x + y
 const sub: MathFunc = (x: number, y: number): number => x - y
 
