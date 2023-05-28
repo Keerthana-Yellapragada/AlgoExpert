c06f5df62d87065dfa1638ca6644d4e9a069d7e6
Keerthana Yellapragada <keerthanay@gmail.com>
2023-05-28 09:46:32 +0800

    binary search method

--- Leetcode/Solutions .py
+++ Leetcode/Solutions .py
@@ -368,3 +368,16 @@ def mySqrt(self, x: int) -> int:
         while number*number<=x:
             number+=1
         return number
+
+    # binary search approach:
+    #  def mySqrtBinary(self, x: int) -> int:
+    #     left,right=1,x
+    #     while left<=right:
+    #         mid=(left+right)//2
+    #         if mid*mid==x:
+    #             return mid
+    #         if mid*mid>x:
+    #             right=mid-1
+    #         else:
+    #             left=mid+1
+    #     return right
