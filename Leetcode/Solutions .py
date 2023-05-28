970d8feaecd65bf565f4256028dc0527903f19bf
Keerthana Yellapragada <keerthanay@gmail.com>
2023-05-28 09:44:17 +0800

    solveD

--- Leetcode/Solutions .py
+++ Leetcode/Solutions .py
@@ -336,3 +336,26 @@ def nextPermutation(self, nums):
 
 
 # Roman to Integer
+def romanToInt(self, s: str) -> int:
+        roman={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        number=0
+        for i in range(len(s)-1):
+            if roman[s[i]] < roman[s[(i+1)]]:
+                number-=roman[s[i]]
+            else:
+                number+=roman[s[i]]
+        return number+roman[s[-1]]
+
+
+def romanToInt(self, s: str) -> int:
+        roman_to_integer = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+        s = s.replace("IV", "IIII").replace("IX", "VIIII").replace("XL", "XXXX").replace("XC", "LXXXX").replace("CD", "CCCC").replace("CM", "DCCCC")
+        return sum(map(lambda x: roman_to_integer[x], s))
