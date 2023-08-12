93bc07ea9a89e9f2350d5cb3d8b8d090d19eba81
Keerthana Yellapragada <keerthanay@gmail.com>
2023-08-12 15:03:44 -0700

    most frequent char

--- Structy/Python Sol.py
+++ Structy/Python Sol.py
@@ -67,3 +67,18 @@ def char_count(s):
     count[char] += 1
 
   return count
+
+
+#  most frequest char
+def most_frequent_char(s):
+  count = {}
+  for char in s:
+    if char not in count:
+      count[char] = 0
+    count[char] += 1
+
+  best = None
+  for char in s:
+    if best is None or count[char] > count[best]:
+      best = char
+  return best
