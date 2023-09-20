366e7f58fe45aab0b407b539d7f39704ee248b98
Keerthana Yellapragada <keerthanay@gmail.com>
2023-09-20 12:09:18 -0700

    python

--- Structy/Python Sol.py
+++ Structy/Python Sol.py
@@ -182,3 +182,17 @@ def intersection(a, b):
     if item in a:
       result.append(item)
   return result
+
+
+def five_sort(nums):
+  i = 0
+  j = len(nums) - 1
+  while i < j:
+    if nums[j] == 5:
+       j -= 1
+    elif nums[i] == 5:
+      nums[i], nums[j] = nums[j], nums[i]
+      i += 1
+    else:
+      i += 1
+  return nums
