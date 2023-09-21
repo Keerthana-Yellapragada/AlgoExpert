40a461b6b242433449e5e7d531ebd2d4c5c6632a
Keerthana Yellapragada <keerthanay@gmail.com>
2023-09-20 18:18:32 -0700

    paired paranthesis

--- Structy/Python Sol.py
+++ Structy/Python Sol.py
@@ -196,3 +196,17 @@ def five_sort(nums):
     else:
       i += 1
   return nums
+
+
+def paired_parentheses(string):
+  count = 0
+
+  for char in string:
+    if char == '(':
+      count += 1
+    elif char == ')':
+      if count == 0:
+        return False
+      count -= 1
+
+  return count == 0
