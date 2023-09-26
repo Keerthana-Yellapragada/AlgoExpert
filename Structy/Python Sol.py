b859be9e758736c84bade07fcee19e0ef5d3f2a0
Keerthana Yellapragada <keerthanay@gmail.com>
2023-09-26 13:45:40 -0700

    get node value:

--- Structy/Python Sol.py
+++ Structy/Python Sol.py
@@ -248,3 +248,18 @@ def linked_list_find(head, target):
     curr = curr.next
 
   return False
+
+
+def get_node_value(head, index):
+
+  curr = head
+  i = 0
+
+  while curr is not None:
+    if i == index:
+      return curr.val
+
+    i += 1
+    curr = curr.next
+
+  return None
