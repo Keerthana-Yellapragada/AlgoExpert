5a5958b3db731a04ee6823164667a899e4fc62c8
Keerthana Yellapragada <keerthanay@gmail.com>
2023-12-02 16:47:23 +0530

    solved

--- Structy/Javascript Sol.js
+++ Structy/Javascript Sol.js
@@ -202,3 +202,29 @@ const insertNode = (head, value, index) => {
     }
     return head;
 };
+
+
+
+const longestStreak = (head) => {
+    let maxStreak = 0;
+    let currentStreak = 0;
+    let currentNode = head;
+    let prevVal = null;
+
+    while (currentNode !== null) {
+        if (currentNode.val === prevVal) {
+            currentStreak += 1;
+        } else {
+            currentStreak = 1;
+        }
+
+        if (currentStreak > maxStreak) {
+            maxStreak = currentStreak;
+        }
+
+        prevVal = currentNode.val;
+        currentNode = currentNode.next;
+    }
+
+    return maxStreak;
+};
