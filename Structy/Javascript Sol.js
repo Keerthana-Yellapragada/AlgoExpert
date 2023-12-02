fbc55548ffbe6c1a0139ee19a67bc9b28c0304db
Keerthana Yellapragada <keerthanay@gmail.com>
2023-12-02 16:47:55 +0530

    recursive sol

--- Structy/Javascript Sol.js
+++ Structy/Javascript Sol.js
@@ -228,3 +228,21 @@ const longestStreak = (head) => {
 
     return maxStreak;
 };
+
+
+
+const mergeLists = (head1, head2) => {
+    if (head1 === null && head2 === null) return null;
+    if (head1 === null) return head2;
+    if (head2 === null) return head1;
+
+    if (head1.val < head2.val) {
+        const next1 = head1.next;
+        head1.next = mergeLists(next1, head2);
+        return head1;
+    } else {
+        const next2 = head2.next;
+        head2.next = mergeLists(head1, next2);
+        return head2;
+    }
+};
