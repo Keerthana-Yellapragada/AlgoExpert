821e35c927ad0281b9db3bf0a46049ce65c49470
Keerthana Yellapragada <keerthanay@gmail.com>
2023-12-11 17:07:08 +0530

    dfs

--- Structy/Javascript Sol.js
+++ Structy/Javascript Sol.js
@@ -244,3 +244,26 @@ const reverseList = (head, prev = null) => {
   head.next = prev;
   return reverseList(next, head);
 };
+
+
+// DFS
+const depthFirstValues = (root) => {
+  if (root === null)
+    return [];
+
+  const values = [];
+  const stack = [root];
+
+  while (stack.length > 0) {
+    const node = stack.pop();
+    values.push(node.val);
+
+    if (node.right !== null)stack.push(node.right);
+
+    if (node.left !== null) stack.push(node.left);
+  }
+
+  return values;
+};
+
+// recursive DFS
