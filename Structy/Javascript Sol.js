25964f9941a12ca668f4d17b770e14b0822189c8
Keerthana Yellapragada <keerthanay@gmail.com>
2024-01-04 13:13:27 +0530

    create combinations

--- Structy/Javascript Sol.js
+++ Structy/Javascript Sol.js
@@ -506,3 +506,24 @@ const permutations = (items) => {
   }
   return fullPermutations;
 };
+
+
+
+// create combinations
+
+const createCombinations = (items, k) => {
+  if (items.length < k) return [];
+
+  if (k === 0) return [
+    []
+  ];
+
+  const first = items[0];
+  const combosWithFirst = [];
+  for (let combo of createCombinations(items.slice(1), k - 1)) {
+    combosWithFirst.push([first, ...combo]);
+  }
+
+  const combosWithoutFirst = createCombinations(items.slice(1), k);
+  return [...combosWithFirst, ...combosWithoutFirst];
+};
