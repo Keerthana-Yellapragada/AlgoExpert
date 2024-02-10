0055f1218203c3b9f65092f8186fcce9b72aedb2
Keerthana Yellapragada <keerthanay@gmail.com>
2024-02-10 14:06:51 -0800

    changed sol

--- Leetcode/Solutions .py
+++ Leetcode/Solutions .py
@@ -337,6 +337,7 @@ def swapPairs(self, head: ListNode) -> ListNode:
 
 # Roman to Integer
 def romanToInt(self, s: str) -> int:
+
         roman={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
         number=0
         for i in range(len(s)-1):
